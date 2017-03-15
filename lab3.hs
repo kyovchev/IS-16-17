4a1d1ced297f7478f48aa06ce33fb27b2d101c3a
kyovchev <kyovchev@gmail.com>
2017-03-15 20:40:24 +0200

    Add iterative solution

--- lab3.hs
+++ lab3.hs
@@ -14,6 +14,9 @@ main = do
   print (sumOfSeriesFast 3 3)
   print ("expected 40")
   print ("---")
+  print (sumOfSeriesIter 3 3)
+  print ("expected 40")
+  print ("---")
 
 -- Задача 1. По зададени x и n, да се изчисли сумата: 1 + x + x^2 + x^3 + ... + x^n
 sumOfSeries :: Double -> Integer -> Double
@@ -26,3 +29,11 @@ sumOfSeriesFast :: Double -> Integer -> Double
 sumOfSeriesFast x n 
  | n == -1 = 0
  | otherwise = 1 + x * sumOfSeriesFast x (n-1)
+
+-- Задача 3. Да се реши задача 1, чрез итерация
+sumOfSeriesIter :: Double -> Integer -> Double
+sumOfSeriesIter x n = helper 0 1 0
+ where
+  helper :: Integer -> Double -> Double -> Double
+  helper i ai sum =
+   if i > n then sum else helper (i + 1) (ai * x) (sum + ai)
